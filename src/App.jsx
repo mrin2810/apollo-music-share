19a5272b4905d3af04f20016d2acf863b7c57248
mrin2810 <mrinmayeerane2810@gmail.com>
2022-02-16 01:08:51 +0530

    UI Ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Grid } from '@material-ui/core';
+import { Grid, useMediaQuery, Hidden } from '@material-ui/core';
 import React from 'react';
 import AddSong from './components/AddSong';
 import Header from './components/Header';
@@ -6,19 +6,29 @@ import SongList from './components/SongList';
 import SongPlayer from './components/SongPlayer';
 
 function App() {
+  const greaterThanMedium = useMediaQuery(theme => theme.breakpoints.up('md'));
+  const greaterThanSmall = useMediaQuery(theme => theme.breakpoints.up('sm'));
+
   return (
     <div style={{ width: '95vw' }}>
-      <Header />
-      <Grid container spacing={3} style={{ paddingTop: '80px' }}>
+      <Hidden only='xs'>
+        <Header />
+      </Hidden>
+      <Grid container spacing={3} style={{ paddingTop: greaterThanSmall ? '80px' : '10px' }}>
         <Grid item xs={12} md={7}>
           <AddSong />
           <SongList />
         </Grid>
-        <Grid style={{
+        <Grid style={
+          greaterThanMedium ? {
           position: 'fixed',
           width: '100%',
           top: '70px',
           right: '0'
+        } : {
+          position: 'fixed',
+          left: 0,
+          bottom: 0,
         }} item xs={12} md={5}>
           <SongPlayer />
         </Grid>
