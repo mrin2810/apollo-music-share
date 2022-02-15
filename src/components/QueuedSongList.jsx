5e2a30841da1fdc332af0fd05d367f235a84af5a
mrin2810 <mrinmayeerane2810@gmail.com>
2022-02-16 00:29:55 +0530

    updated theme colors

--- src/components/QueuedSongList.jsx
+++ src/components/QueuedSongList.jsx
@@ -1,7 +1,12 @@
+import { Typography } from '@material-ui/core';
 import React from 'react';
 
 function QueuedSongList() {
-    return <div>QueuedSongList</div>
+    return (
+        <div style={{margin: '10px 0'}}>
+            <Typography color='textSecondary'></Typography>
+        </div>
+    )
 }
 
 export default QueuedSongList;
--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,11 @@
-import { indigo, deepPurple } from "@material-ui/core/colors";
+import { green, red } from "@material-ui/core/colors";
 import { createTheme } from "@material-ui/core/styles";
 
 const theme = createTheme({
     palette: {
         type: 'dark',
-        primary: indigo,
-        secondary: deepPurple
+        primary: green,
+        secondary: red
     }
 })
 
